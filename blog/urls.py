5e49e7e2447fba77b21ce076aa8dc2b2d07a3b2c
@TakumaOkubo <b25a0068@tcs-one.com>
2025-12-03 15:36:37 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views     #blogアプリの全てのビューをインポートす
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
